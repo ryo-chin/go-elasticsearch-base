37c9bb3eb2112df2ef3972a9a209013896e91c4b
RyoheiYamamoto <ryohei.yamamoto@bizreach.co.jp>
2019-03-01 18:14:05 +0900

    feature: select from rdb.

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
+
+var svc *sqs.SQS
 
 func main() {
 	//
@@ -18,8 +24,11 @@ func main() {
 		Credentials: credentials.AnonymousCredentials,
 		Region:      aws.String("elasticmq")},
 	)
-	svc := sqs.New(sess)
+	svc = sqs.New(sess)
 	qURL := "http://localhost:9324/queue/user"
+
+	targetUserIdMap := map[string]string{}
+	defer RecoverId(qURL, targetUserIdMap)
 
 	result, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
 		AttributeNames: []*string{
@@ -42,4 +51,54 @@ func main() {
 		return
 	}
 	fmt.Printf("Success: %+v\n", result.Messages)
+
+	for _, message := range result.Messages {
+		targetUserIdMap[*message.MessageId] = *message.Body
+	}
+
+	//
+	// Select from MySQL
+	//
+	dbUser := "go_elastic_user"
+	password := "password"
+	protocol := "tcp(localhost:3309)"
+	dbName := "go_elastic_db"
+	db, err := gorm.Open("mysql", dbUser+":"+password+"@"+protocol+"/"+dbName)
+	defer CloseDB(*db, err)
+
+	var users []User
+	var targetUserIdList []int
+	for _, v := range targetUserIdMap {
+		i, _ := strconv.Atoi(v)
+		targetUserIdList = append(targetUserIdList, i)
+	}
+	db.Where("id in (?)", targetUserIdList).Find(&users)
+	fmt.Printf("Success: %+v\n", users)
+}
+
+func CloseDB(db gorm.DB, err error) {
+	fmt.Println("Error", err)
+	db.Close()
+}
+
+func RecoverId(qURL string, targetUserIdMap map[string]string) {
+	var batchRequestEntryList []*sqs.SendMessageBatchRequestEntry
+	for k, v := range targetUserIdMap {
+		entry := sqs.SendMessageBatchRequestEntry{Id: &k, MessageBody: &v}
+		batchRequestEntryList = append(batchRequestEntryList, &entry)
+	}
+	svc.SendMessageBatchRequest(&sqs.SendMessageBatchInput{
+		Entries:  batchRequestEntryList,
+		QueueUrl: &qURL,
+	})
+}
+
+type User struct {
+	Id          int
+	LastName    string
+	FirstName   string
+	Gender      string
+	PhoneNumber string
+	Email       string
+	Password    string
 }
