87cbfd416315ecd18dcbd72d89d5ba624922c570
RyoheiYamamoto <ryohei.yamamoto@bizreach.co.jp>
2019-03-01 16:32:22 +0900

    feature: dequeue id from sqs.

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
 func main() {
-	println("Dequeue id from SQS")
-	println("Select from RDB")
-	println("Index to Elasticsearch")
+	//
+	// Deque ID from SQS
+	//
+	sess, err := session.NewSession(&aws.Config{
+		Endpoint:    aws.String("http://localhost:9324"),
+		Credentials: credentials.AnonymousCredentials,
+		Region:      aws.String("elasticmq")},
+	)
+	svc := sqs.New(sess)
+	qURL := "http://localhost:9324/queue/user"
+
+	result, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
+		AttributeNames: []*string{
+			aws.String(sqs.MessageSystemAttributeNameSentTimestamp),
+		},
+		MessageAttributeNames: []*string{
+			aws.String(sqs.QueueAttributeNameAll),
+		},
+		QueueUrl:            &qURL,
+		MaxNumberOfMessages: aws.Int64(10),
+		VisibilityTimeout:   aws.Int64(60), // 60 seconds
+		WaitTimeSeconds:     aws.Int64(0),
+	})
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+	if len(result.Messages) == 0 {
+		fmt.Println("Received no messages")
+		return
+	}
+	fmt.Printf("Success: %+v\n", result.Messages)
 }
